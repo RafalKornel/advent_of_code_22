315174bfdf51510652ff2d5f11cf08df12e813ad
RafalKornel <rafal.kornel123@gmail.com>
2023-08-03 14:52:10 +0200

    fix: update Heap import

--- day7/buildDirectoriesSizeMinHeap.ts
+++ day7/buildDirectoriesSizeMinHeap.ts
@@ -1,7 +1,7 @@
 import { Dir } from "./types.ts";
 
 // https://github.com/RafalKornel/algs/blob/main/structures/heap/Heap.ts
-import { Heap } from "https://raw.githubusercontent.com/RafalKornel/algs/main/structures/heap/Heap.ts";
+import { Heap } from "https://raw.githubusercontent.com/RafalKornel/algs/main/heap/Heap.ts";
 
 export function buildDirectoriesSizeMinHeap(root: Dir): Heap<"min"> {
   const heap = new Heap("min");
